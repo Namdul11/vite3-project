1a4cc2f2eaa0f0e7d2aa3c3dba92b53453a4789a
NAMDUL LAMA <john.namdul@gmail.com>
2026-02-13 18:16:18 +0545

    api integration

--- src/component/product/CreateProduct.jsx
+++ src/component/product/CreateProduct.jsx
@@ -1,81 +1,117 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const CreateProduct = () => {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [quantity, setQuantity] = useState("");
-  const [isDamage, setIsDamage] = useState(false);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [isDamage, setIsDamage] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = {
-      name: name,
-      price: price,
-      quantity: quantity,
-      isDamage: isDamage,
-    };
-    console.log(data);
-  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      name: name,
+      price: price,
+      quantity: quantity,
+      isDamage: isDamage,
+    };
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <div>
-            <label htmlFor="name">Name :</label>
-            <input
-              type="text"
-              name=""
-              id="name"
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-            />
-          </div>
-          <div>
-            <label htmlFor="price">Price :</label>
-            <input
-              type="text"
-              name=""
-              id="price"
-              value={price}
-              onChange={(e) => {
-                setPrice(e.target.value);
-              }}
-            />
-          </div>
-          <div>
-            <label htmlFor="quantity">Quantity :</label>
-            <input
-              type="text"
-              name=""
-              id="quantity"
-              value={quantity}
-              onChange={(e) => {
-                setQuantity(e.target.value);
-              }}
-            />
-          </div>
-          <div>
-            <label htmlFor="isDamage">isDamage :</label>
-            <input
-              type="checkbox"
-              name=""
-              id="isDamage"
-              checked={isDamage}
-              onChange={(e) => {
-                setIsDamage(e.target.checked);
-              }}
-            />
-          </div>
-        </div>
-        <div>
-          <button>Submit</button>
-        </div>
-      </form>
-    </>
-  );
+    try {
+      let result = await axios({
+        url: "http://localhost:8000/product",
+        method: "post",
+        data: data,
+      });
+      setName("");//value haru backend ko clear  bayera tala toast message auna lai
+      setPrice("");
+      setQuantity("");
+      setIsDamage(false);
+      toast.success(result.data.message);//backend bitra ko result ->data bita ko->message
+      //console.log(result);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  return (
+    <>
+            
+      <form onSubmit={handleSubmit}>
+                
+        <div>
+                    
+          <div>
+                        <label htmlFor="name">Name :</label>
+                        
+            <input
+              type="text"
+              name=""
+              id="name"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+                      
+          </div>
+                    
+          <div>
+                        <label htmlFor="price">Price :</label>
+                        
+            <input
+              type="text"
+              name=""
+              id="price"
+              value={price}
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+            />
+                      
+          </div>
+                    
+          <div>
+                        <label htmlFor="quantity">Quantity :</label>
+                        
+            <input
+              type="text"
+              name=""
+              id="quantity"
+              value={quantity}
+              onChange={(e) => {
+                setQuantity(e.target.value);
+              }}
+            />
+                      
+          </div>
+                    
+          <div>
+                        <label htmlFor="isDamage">isDamage :</label>
+                        
+            <input
+              type="checkbox"
+              name=""
+              id="isDamage"
+              checked={isDamage}
+              onChange={(e) => {
+                setIsDamage(e.target.checked);
+              }}
+            />
+                      
+          </div>
+                  
+        </div>
+                
+        <div>
+                    <button>Submit</button>
+                  
+        </div>
+              
+      </form>
+          
+    </>
+  );
 };
 
 export default CreateProduct;
--- src/component/user/ReadAllUser.jsx
+++ src/component/user/ReadAllUser.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const ReadAllUser = () => {
-  return <div>read user</div>;
+  let users = [
+    {
+      name: "namdul",
+      email: "abcd@gmail.com",
+      profileImage: "aaaa",
+    },
+    {
+      name: "ram",
+      email: "ram@gmail.com",
+      profileImage: "aaaa",
+    },
+    {
+      name: "hari",
+      email: "hari@gmail.com",
+      profileImage: "aaaa",
+    },
+  ];
+  return (
+    <div>
+      {users.map((item, i) => {
+        return (
+          <div key={i} className="card">
+            <p>name is {item.name}</p>
+            <p>email is {item.email}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ReadAllUser;
